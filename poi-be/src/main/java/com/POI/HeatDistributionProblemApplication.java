aafc695522ea5f36b30dff46132494c27ae0b4fb
Stanislav Stoychev <stas2000@abv.bg>
2023-04-15 13:07:08 +0300

    Initial Commit

---
+++ poi-be/src/main/java/com/POI/HeatDistributionProblemApplication.java
@@ -0,0 +1,13 @@
+package com.POI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatDistributionProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatDistributionProblemApplication.class, args);
+	}
+
+}
---
+++ poi-be/src/test/java/com/POI/HeatDistributionProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.POI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatDistributionProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
